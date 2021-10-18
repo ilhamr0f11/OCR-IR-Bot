d0535ed7c967ae94713b6d04a5d76f5a558e5f39
Ilham Rofi'i <70249693+ilhamr0f11@users.noreply.github.com>
2021-10-18 16:47:43 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-welcome_text = 'Hi!\nWelcome to Optical Character Recognizer Bot.\nJust send a clear image to me and I will recognize the text in the image and send it as a message!'
+welcome_text = 'Hi!\nWelcome to Optical Character Recognizer Bot.\nJust send a clear image to me and I will recognize the text in the image and send it as a message!\nChannel:IrBotsUpdate'
 reply_to_text_message = 'Hey, I\'m not designed to reply to text messages 😕\nHit /start and read the instructions!'
 no_text_found = 'It\'s time to get my eye 👀 tested\nI\'m not able to read any text in this image 😔\nTrust me, I even tried with my 👓 on!'
